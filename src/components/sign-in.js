05f5d9bf2328fa058ca920342c7b7fa1fb82e0a8
wallacesintra <wallvxwallace962@gmail.com>
2023-10-21 22:40:35 +0300

    improved input UI

--- src/components/sign-in.js
+++ src/components/sign-in.js
@@ -1,11 +1,77 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet} from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, TextInput} from "react-native";
+import { FontAwesome, Entypo } from '@expo/vector-icons';
+
 const  SignIn= () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style = {styles.container}>
+            <View >
+                <Text style = {styles.title}>Create account</Text>
+                <View style = {styles.inputContainer}>
+                    <Entypo name="user" size={20} color="black" style = {styles.icon}/>
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="Username"
+                    />
+                </View>
+
+                <View style = {styles.inputContainer}>
+                    <Entypo name="lock" size={20} color="black" style = {styles.icon}/>
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="Password"
+                        secureTextEntry = {true}
+                    />
+                </View>
+
+                <View style = {styles.inputContainer}>
+                    <Entypo name="mail" size={20} color="black" style = {styles.icon}/>
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="Email"
+                    />
+                </View>
+
+                <View style = {styles.inputContainer}>
+                    <Entypo name="phone" size={20} color="black" style = {styles.icon}/>
+                    <TextInput
+                        style = {styles.input}
+                        placeholder="Mobile"
+                    />
+                </View>
+            </View>
 
         </SafeAreaView>
 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        //justifyContent: 'center'
+    },
+    title: {
+        alignItems: 'center',
+        fontSize: 30
+    },
+    inputContainer: {
+        margin: 10,
+        padding: 9,
+        alignItems: 'center',
+        width: 320,
+        borderWidth: 1,
+        borderRadius: 20,
+        backgroundColor: 'white',
+        flexDirection: 'row'
+    }, 
+    icon: {
+        padding: 3,
+    },
+    input: {
+        fontSize: 20,
+        marginLeft: 7,
+    }
+})
 export default SignIn
